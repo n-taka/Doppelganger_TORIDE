fb638a007f49a289cdfcc2da21ee4249069420a8
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2022-02-06 14:59:13 +0900

    [impl] Basic/meshErrorInfo

--- plugins/Basic/meshErrorInfo/meshErrorInfo.cpp
+++ plugins/Basic/meshErrorInfo/meshErrorInfo.cpp
@@ -1,18 +1,11 @@
 #ifndef MESHERRORINFO_CPP
 #define MESHERRORINFO_CPP
 
-#if defined(_WIN64)
-#define DLLEXPORT __declspec(dllexport)
-#elif defined(__APPLE__)
-#define DLLEXPORT __attribute__((visibility("default")))
-#elif defined(__linux__)
-#define DLLEXPORT __attribute__((visibility("default")))
-#endif
+#include "pluginCommon.h"
 
 #include <memory>
 #include <nlohmann/json.hpp>
 
-#include "Doppelganger/Room.h"
 #include "Doppelganger/triangleMesh.h"
 
 #include "igl/boundary_facets.h"
@@ -20,9 +13,34 @@
 #include "igl/is_vertex_manifold.h"
 
 #include <string>
-#include <mutex>
 
-extern "C" DLLEXPORT void pluginProcess(const std::shared_ptr<Doppelganger::Room> &room, const nlohmann::json &parameters, nlohmann::json &response, nlohmann::json &broadcast)
+void getPtrStrArrayForPartialConfig(
+	const char *&parameterChar,
+	char *&ptrStrArrayCoreChar,
+	char *&ptrStrArrayRoomChar)
+{
+	const nlohmann::json parameter = nlohmann::json::parse(parameterChar);
+
+	nlohmann::json ptrStrArrayCore = nlohmann::json::array();
+	writeJSONToChar(ptrStrArrayCoreChar, ptrStrArrayCore);
+	nlohmann::json ptrStrArrayRoom = nlohmann::json::array();
+	for (const auto &UUID : parameter.at("meshes"))
+	{
+		std::string targetMeshPath("/meshes/");
+		targetMeshPath += UUID.get<std::string>();
+		ptrStrArrayRoom.push_back(targetMeshPath);
+	}
+	writeJSONToChar(ptrStrArrayRoomChar, ptrStrArrayRoom);
+}
+
+void pluginProcess(
+	const char *&configCoreChar,
+	const char *&configRoomChar,
+	const char *&parameterChar,
+	char *&configCorePatchChar,
+	char *&configRoomPatchChar,
+	char *&responseChar,
+	char *&broadcastChar)
 {
 	////
 	// [IN]
@@ -49,35 +67,35 @@ extern "C" DLLEXPORT void pluginProcess(const std::shared_ptr<Doppelganger::Room
 	// }
 
 	// create empty response/broadcast
-	response = nlohmann::json::object();
-	broadcast = nlohmann::json::object();
+	// initialize
+	const nlohmann::json configRoom = nlohmann::json::parse(configRoomChar);
+	const nlohmann::json parameter = nlohmann::json::parse(parameterChar);
+	nlohmann::json response = nlohmann::json::object();
 
 	response["meshes"] = nlohmann::json::object();
+	for (const auto &UUID : parameter.at("meshes"))
 	{
-		std::lock_guard<std::mutex> lock(room->mutexMeshes);
+		const std::string meshUUID = UUID.get<std::string>();
+		const Doppelganger::TriangleMesh mesh = configRoom.at("meshes").at(meshUUID).get<Doppelganger::TriangleMesh>();
 
-		for (const auto &UUID : parameters.at("meshes"))
+		nlohmann::json errorInfo = nlohmann::json::object();
 		{
-			const std::string &meshUUID = UUID.get<std::string>();
-			const std::shared_ptr<Doppelganger::triangleMesh> &mesh = room->meshes.at(meshUUID);
-
-			nlohmann::json errorInfo = nlohmann::json::object();
-			{
-				Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic> BF;
-				igl::boundary_facets(mesh->F, BF);
-				errorInfo["closed"] = (BF.rows() == 0);
-			}
-			{
-				errorInfo["edgeManifold"] = igl::is_edge_manifold(mesh->F);
-			}
-			{
-				Eigen::Matrix<int, Eigen::Dynamic, 1> B;
-				errorInfo["vertexManifold"] = igl::is_vertex_manifold(mesh->F, B);
-			}
-
-			response.at("meshes")[meshUUID] = errorInfo;
+			Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic> BF;
+			igl::boundary_facets(mesh.F_, BF);
+			errorInfo["closed"] = (BF.rows() == 0);
 		}
+		{
+			errorInfo["edgeManifold"] = igl::is_edge_manifold(mesh.F_);
+		}
+		{
+			Eigen::Matrix<int, Eigen::Dynamic, 1> B;
+			errorInfo["vertexManifold"] = igl::is_vertex_manifold(mesh.F_, B);
+		}
+
+		response.at("meshes")[meshUUID] = errorInfo;
 	}
+
+	writeJSONToChar(responseChar, response);
 }
 
 #endif
