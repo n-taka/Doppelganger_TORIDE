a6797c84cc781c6ca560b281433f79e784231d27
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2022-03-21 16:12:50 +0900

    [misc] update build script

--- build.sh
+++ build.sh
@@ -26,13 +26,11 @@ if [ "$(uname)" == "Darwin" ]; then
     cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${triplet}" -DCMAKE_BUILD_TYPE="${CONFIG}"
     cmake --build build --config "${CONFIG}"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    subst X: .
-    cd X:
+    subst X: submodule/Doppelganger_Util/submodule
     cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${triplet}" -DCMAKE_BUILD_TYPE="${CONFIG}"
     cmake --build build --config "${CONFIG}"
     # revert subst command
     # "/" symbol was comprehended as separator for path in MINGW. Thus, we need to explicitly use "//"
-    # echo "unbind ./submodule as X:"
     subst X: //D
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${triplet}" -DCMAKE_BUILD_TYPE="${CONFIG}"
