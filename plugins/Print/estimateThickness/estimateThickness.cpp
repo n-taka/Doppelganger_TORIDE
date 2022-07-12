8e06c8d4bf0125b142529d903e54ab72694cfe16
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2022-07-12 15:19:46 +0900

    [impl] Print/estimateThickness

--- plugins/Print/estimateThickness/estimateThickness.cpp
+++ plugins/Print/estimateThickness/estimateThickness.cpp
@@ -1,28 +1,48 @@
-#ifndef HOLLOWMESH_CPP
-#define HOLLOWMESH_CPP
+#ifndef ESTIMATETHICKNESS_CPP
+#define ESTIMATETHICKNESS_CPP
 
-#if defined(_WIN64)
-#define DLLEXPORT __declspec(dllexport)
-#elif defined(__APPLE__)
-#define DLLEXPORT __attribute__((visibility("default")))
-#elif defined(__linux__)
-#define DLLEXPORT __attribute__((visibility("default")))
-#endif
+#include "Doppelganger/pluginCommon.h"
 
 #include <memory>
 #include <nlohmann/json.hpp>
 
-#include "Doppelganger/Room.h"
-#include "Doppelganger/triangleMesh.h"
+#include "Doppelganger/TriangleMesh.h"
+#include "Doppelganger/Util/storeHistory.h"
 
 #include <string>
-#include <mutex>
 #include <vector>
 
 #include "igl/per_vertex_normals.h"
 #include "igl/embree/shape_diameter_function.h"
 
-extern "C" DLLEXPORT void pluginProcess(const std::shared_ptr<Doppelganger::Room> &room, const nlohmann::json &parameters, nlohmann::json &response, nlohmann::json &broadcast)
+void getPtrStrArrayForPartialConfig(
+	const char *&parameterChar,
+	char *&ptrStrArrayCoreChar,
+	char *&ptrStrArrayRoomChar)
+{
+	const nlohmann::json parameter = nlohmann::json::parse(parameterChar);
+
+	nlohmann::json ptrStrArrayCore = nlohmann::json::array();
+	writeJSONToChar(ptrStrArrayCoreChar, ptrStrArrayCore);
+	nlohmann::json ptrStrArrayRoom = nlohmann::json::array();
+	ptrStrArrayRoom.push_back("/history");
+	for (const auto &UUID : parameter.at("meshes"))
+	{
+		std::string targetMeshPath("/meshes/");
+		targetMeshPath += UUID.get<std::string>();
+		ptrStrArrayRoom.push_back(targetMeshPath);
+	}
+	writeJSONToChar(ptrStrArrayRoomChar, ptrStrArrayRoom);
+}
+
+void pluginProcess(
+	const char *&configCoreChar,
+	const char *&configRoomChar,
+	const char *&parameterChar,
+	char *&configCorePatchChar,
+	char *&configRoomPatchChar,
+	char *&responseChar,
+	char *&broadcastChar)
 {
 	////
 	// [IN]
@@ -32,7 +52,8 @@ extern "C" DLLEXPORT void pluginProcess(const std::shared_ptr<Doppelganger::Room
 	// 	 "UUID-B",
 	//   ...
 	//  ],
-	//  "acceptableThickness": double for minimum acceptable thickness
+	//  "acceptableThickness": double for minimum acceptable thickness,
+	//  "sampleCount": #samples for shape diameter calculation
 	// }
 
 	// [OUT]
@@ -44,65 +65,78 @@ extern "C" DLLEXPORT void pluginProcess(const std::shared_ptr<Doppelganger::Room
 	//  }
 	// }
 
-	// create empty response/broadcast
-	response = nlohmann::json::object();
-	broadcast = nlohmann::json::object();
-	nlohmann::json diff = nlohmann::json::object();
-	nlohmann::json diffInv = nlohmann::json::object();
-
+	// initialize
+	const nlohmann::json configRoom = nlohmann::json::parse(configRoomChar);
+	const nlohmann::json parameter = nlohmann::json::parse(parameterChar);
+	nlohmann::json configRoomPatch = nlohmann::json::object();
+	nlohmann::json broadcast = nlohmann::json::object();
 	broadcast["meshes"] = nlohmann::json::object();
-	diff["meshes"] = nlohmann::json::object();
-	diffInv["meshes"] = nlohmann::json::object();
+	configRoomPatch["meshes"] = nlohmann::json::object();
+
+	const double acceptableThickness = parameter.at("acceptableThickness").get<double>();
+	const int sampleCount = parameter.at("sampleCount").get<int>();
+
+	for (const auto &UUID : parameter.at("meshes"))
 	{
-		std::lock_guard<std::mutex> lock(room->mutexMeshes);
+		const std::string meshUUID = UUID.get<std::string>();
+		Doppelganger::TriangleMesh mesh = configRoom.at("meshes").at(meshUUID).get<Doppelganger::TriangleMesh>();
 
-		const double &acceptableThickness = parameters.at("acceptableThickness").get<double>();
-		const int &sampleCount = parameters.at("sampleCount").get<int>();
+		nlohmann::json diff = nlohmann::json::object();
+		nlohmann::json diffInv = nlohmann::json::object();
+		diff["meshes"] = nlohmann::json::object();
+		diffInv["meshes"] = nlohmann::json::object();
 
-		// remove mesh
-		for (const auto &UUID : parameters.at("meshes"))
+		// store current mesh
+		diffInv.at("meshes")[mesh.UUID_] = configRoom.at("meshes").at(meshUUID);
+
+		// estimate thickness
 		{
-			const std::string &meshUUID = UUID.get<std::string>();
-			const std::shared_ptr<Doppelganger::triangleMesh> &mesh = room->meshes.at(meshUUID);
-			// store current mesh
-			diffInv.at("meshes")[meshUUID] = mesh->dumpToJson(false);
-			diffInv.at("meshes").at(meshUUID)["remove"] = false;
-
 			igl::embree::EmbreeIntersector ei;
-			ei.init(mesh->V.template cast<float>(), mesh->F, true);
+			ei.init(mesh.V_.template cast<float>(), mesh.F_, true);
 
-			Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> P, N;
-			P = mesh->V;
-			igl::per_vertex_normals(mesh->V, mesh->F, N);
+			Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> P;
+			P = mesh.V_;
+			igl::per_vertex_normals(mesh.V_, mesh.F_, mesh.VN_);
 			Eigen::Matrix<double, Eigen::Dynamic, 1> S;
-			igl::embree::shape_diameter_function(ei, P, N, sampleCount, S);
+			igl::embree::shape_diameter_function(ei, P, mesh.VN_, sampleCount, S);
 
-			mesh->VC.resizeLike(mesh->V);
-			for (int v = 0; v < mesh->VC.rows(); ++v)
+			mesh.VC_.resizeLike(mesh.V_);
+			for (int v = 0; v < mesh.VC_.rows(); ++v)
 			{
 				if (S(v, 0) < acceptableThickness)
 				{
 					// orange
-					mesh->VC.row(v) << (245.0 / 255.0),
+					mesh.VC_.row(v) << (245.0 / 255.0),
 						(108.0 / 255.0),
 						(10.0 / 255.0);
 				}
 				else
 				{
 					// gray
-					mesh->VC.row(v) << (220.0 / 255.0),
+					mesh.VC_.row(v) << (220.0 / 255.0),
 						(220.0 / 255.0),
 						(220.0 / 255.0);
 				}
 			}
-
-			broadcast["meshes"][meshUUID] = mesh->dumpToJson(true);
-			broadcast["meshes"][meshUUID]["remove"] = false;
-			diff["meshes"][meshUUID] = mesh->dumpToJson(false);
-			diff["meshes"][meshUUID]["remove"] = false;
 		}
+
+		// store modified mesh
+		diff.at("meshes")[mesh.UUID_] = mesh;
+		configRoomPatch.at("meshes")[mesh.UUID_] = mesh;
+
+		// update broadcast
+		nlohmann::json meshJsonf;
+		Doppelganger::to_json(meshJsonf, mesh, true);
+		broadcast.at("meshes")[mesh.UUID_] = meshJsonf;
+
+		// store history
+		configRoomPatch["history"] = nlohmann::json::object();
+		Doppelganger::Util::storeHistory(configRoom.at("history"), diff, diffInv, configRoomPatch.at("history"));
 	}
-	room->storeHistory(diff, diffInv);
+
+	// write result
+	writeJSONToChar(configRoomPatchChar, configRoomPatch);
+	writeJSONToChar(broadcastChar, broadcast);
 }
 
 #endif
